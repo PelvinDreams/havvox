82f3eb4a471b83dc4d9ce0fb25fa7704a64034b8
Uwem Precious <155549089+PelvinDreams@users.noreply.github.com>
2024-09-29 15:16:57 -0700

    initiated routes

--- Day4/app.js
+++ Day4/app.js
@@ -1,7 +1,11 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 5000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get('/api', (req, res) => {
   res.json(`HTTP GET request received`);
@@ -10,9 +14,32 @@ app.get('/api', (req, res) => {
 app.use(express.static(path.join(__dirname, 'public')));
  
 // Home Route
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html')); // Use path to specify the correct file location
 });
+
+// GET ROUTE
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+
+
+
+// POST ROUTE
+app.post("/register", (req, res) => {
+
+});
+
+
+app.post("/login", (req, res) => {
+  
+});
+
 
 app.use(function(req, res) {
   res.status(400);
